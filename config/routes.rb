9820f36dff9643cb0b5124aa329f8228885f46e0
namelos <namelos@outlook.com>
2016-03-17 23:49:10 +0800

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'user#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
